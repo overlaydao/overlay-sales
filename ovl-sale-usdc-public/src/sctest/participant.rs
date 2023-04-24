6c8ee8d3dba3dc7b9594f01e23ff6de9eaacbc32
daratao <newsnow.bck@gmail.com>
2023-04-24 21:57:59 +0900

    refactor for testing utility

--- ovl-sale-usdc-public/src/sctest/participant.rs
+++ ovl-sale-usdc-public/src/sctest/participant.rs
@@ -2,73 +2,8 @@ use concordium_std::concordium_cfg_test;
 
 #[concordium_cfg_test]
 mod tests {
-    #![allow(unused)]
 
-    use crate::*;
-    use concordium_std::test_infrastructure::*;
-
-    const USDC: ContractAddress = ContractAddress {
-        index: 1,
-        subindex: 0,
-    };
-    const SELF_ADDRESS: ContractAddress = ContractAddress {
-        index: 10,
-        subindex: 0,
-    };
-    const PJ_TOKEN: ContractAddress = ContractAddress {
-        index: 11,
-        subindex: 0,
-    };
-    const OVL_TEAM_ACC: AccountAddress = AccountAddress([0u8; 32]);
-    const PJ_ADMIN_ACC: AccountAddress = AccountAddress([1u8; 32]);
-    const ADDR_OVL: Address = Address::Account(AccountAddress([2u8; 32]));
-    const ADDR_BBB: Address = Address::Contract(ContractAddress {
-        index: 100,
-        subindex: 0,
-    });
-
-    fn def_sale_schedule() -> SaleSchedule {
-        SaleSchedule {
-            open_at: BTreeMap::from([
-                (Timestamp::from_timestamp_millis(10), Prior::TOP),
-                (Timestamp::from_timestamp_millis(20), Prior::SECOND),
-            ]),
-            close_at: Timestamp::from_timestamp_millis(30),
-            vesting_start: None,
-            vesting_period: BTreeMap::from([
-                (Duration::from_days(1), 25),
-                (Duration::from_days(2), 40),
-                (Duration::from_days(3), 35),
-            ]),
-        }
-    }
-
-    fn def_sale_info(applied_units: u32) -> SaleInfo {
-        SaleInfo {
-            price_per_token: 5_000_000,
-            token_per_unit: ContractTokenAmount::from(200),
-            max_units: 2,
-            min_units: 1,
-            applied_units,
-        }
-    }
-
-    fn receive_context(
-        owner: AccountAddress,
-        invoker: AccountAddress,
-        sender: Address,
-        slot_time: SlotTime,
-        parameter_bytes: &[u8],
-    ) -> TestReceiveContext {
-        let mut ctx = TestReceiveContext::empty();
-        ctx.set_self_address(SELF_ADDRESS);
-        ctx.set_metadata_slot_time(slot_time);
-        ctx.set_owner(owner);
-        ctx.set_invoker(invoker);
-        ctx.set_sender(sender);
-        ctx.set_parameter(parameter_bytes);
-        ctx
-    }
+    use crate::{sctest::*, *};
 
     #[concordium_test]
     fn test_user_deposit() {
@@ -350,7 +285,13 @@ mod tests {
             bbb_claimed_inc: 0,
             project_token: None,
             schedule: def_sale_schedule(),
-            saleinfo: def_sale_info(0),
+            saleinfo: SaleInfo {
+                price_per_token: 5_000_000,
+                token_per_unit: ContractTokenAmount::from(200),
+                max_units: 2,
+                min_units: 1,
+                applied_units: 0,
+            },
             participants: state_builder.new_map(),
         };
 
@@ -399,7 +340,13 @@ mod tests {
             bbb_claimed_inc: 0,
             project_token: None,
             schedule: def_sale_schedule(),
-            saleinfo: def_sale_info(2),
+            saleinfo: SaleInfo {
+                price_per_token: 5_000_000,
+                token_per_unit: ContractTokenAmount::from(200),
+                max_units: 2,
+                min_units: 1,
+                applied_units: 2,
+            },
             participants: expected_participants,
         };
 
--- ovl-sale-usdc-public/src/sctest/project_admin.rs
+++ ovl-sale-usdc-public/src/sctest/project_admin.rs
@@ -2,77 +2,8 @@ use concordium_std::concordium_cfg_test;
 
 #[concordium_cfg_test]
 mod tests {
-    #![allow(unused)]
 
-    use crate::*;
-    use concordium_std::test_infrastructure::*;
-
-    const USDC: ContractAddress = ContractAddress {
-        index: 1,
-        subindex: 0,
-    };
-
-    const SELF_ADDRESS: ContractAddress = ContractAddress {
-        index: 10,
-        subindex: 0,
-    };
-
-    const PJ_TOKEN: ContractAddress = ContractAddress {
-        index: 11,
-        subindex: 0,
-    };
-    const OVL_TEAM_ACC: AccountAddress = AccountAddress([0u8; 32]);
-    const OVL_TEAM_ADDR: Address = Address::Account(OVL_TEAM_ACC);
-    const PJ_ADMIN_ACC: AccountAddress = AccountAddress([1u8; 32]);
-    const PJ_ADMIN_ADDR: Address = Address::Account(PJ_ADMIN_ACC);
-    const ADDR_OVL: Address = Address::Account(AccountAddress([2u8; 32]));
-    const ADDR_BBB: Address = Address::Contract(ContractAddress {
-        index: 100,
-        subindex: 0,
-    });
-
-    fn def_sale_schedule() -> SaleSchedule {
-        SaleSchedule {
-            open_at: BTreeMap::from([
-                (Timestamp::from_timestamp_millis(10), Prior::TOP),
-                (Timestamp::from_timestamp_millis(20), Prior::SECOND),
-            ]),
-            close_at: Timestamp::from_timestamp_millis(30),
-            vesting_start: None,
-            vesting_period: BTreeMap::from([
-                (Duration::from_days(1), 25),
-                (Duration::from_days(2), 40),
-                (Duration::from_days(3), 35),
-            ]),
-        }
-    }
-
-    fn def_sale_info(applied_units: u32) -> SaleInfo {
-        SaleInfo {
-            price_per_token: 5_000_000,
-            token_per_unit: ContractTokenAmount::from(200),
-            max_units: 100,
-            min_units: 1,
-            applied_units,
-        }
-    }
-
-    fn receive_context(
-        owner: AccountAddress,
-        invoker: AccountAddress,
-        sender: Address,
-        slot_time: SlotTime,
-        parameter_bytes: &[u8],
-    ) -> TestReceiveContext {
-        let mut ctx = TestReceiveContext::empty();
-        ctx.set_self_address(SELF_ADDRESS);
-        ctx.set_metadata_slot_time(slot_time);
-        ctx.set_owner(owner);
-        ctx.set_invoker(invoker);
-        ctx.set_sender(sender);
-        ctx.set_parameter(parameter_bytes);
-        ctx
-    }
+    use crate::{sctest::*, *};
 
     #[concordium_test]
     fn test_proj_claim() {
@@ -173,7 +104,7 @@ mod tests {
         let ctx = receive_context(
             OVL_TEAM_ACC,
             OVL_TEAM_ACC,
-            OVL_TEAM_ADDR,
+            Address::from(OVL_TEAM_ACC),
             Timestamp::from_timestamp_millis(35),
             &[],
         );
@@ -184,7 +115,7 @@ mod tests {
         let ctx = receive_context(
             OVL_TEAM_ACC,
             PJ_ADMIN_ACC,
-            PJ_ADMIN_ADDR,
+            Address::from(PJ_ADMIN_ACC),
             Timestamp::from_timestamp_millis(40),
             &[],
         );
@@ -253,7 +184,7 @@ mod tests {
         let ctx = receive_context(
             OVL_TEAM_ACC,
             PJ_ADMIN_ACC,
-            PJ_ADMIN_ADDR,
+            Address::from(PJ_ADMIN_ACC),
             Timestamp::from_timestamp_millis(40),
             &[],
         );
