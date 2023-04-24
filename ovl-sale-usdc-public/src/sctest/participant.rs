e36086dc4ff28fb3eec23d6be97382ac4e40ac86
daratao <newsnow.bck@gmail.com>
2023-04-24 10:53:59 +0900

    add test for user deposit when no room

--- ovl-sale-usdc-public/src/sctest/participant.rs
+++ ovl-sale-usdc-public/src/sctest/participant.rs
@@ -330,4 +330,143 @@ mod tests {
             "state has been changed unexpectedly..."
         );
     }
+
+    #[concordium_test]
+    fn test_user_deposit_when_no_room() {
+        let acc1 = AccountAddress([11u8; 32]);
+        let acc2 = AccountAddress([12u8; 32]);
+        let acc3 = AccountAddress([13u8; 32]);
+
+        let mut state_builder = TestStateBuilder::new();
+
+        let mut state = State {
+            usdc_contract: USDC,
+            proj_admin: PJ_ADMIN_ACC,
+            status: SaleStatus::Ready,
+            paused: false,
+            addr_ovl: ADDR_OVL,
+            addr_bbb: ADDR_BBB,
+            ovl_claimed_inc: 0,
+            bbb_claimed_inc: 0,
+            project_token: None,
+            schedule: def_sale_schedule(),
+            saleinfo: def_sale_info(0),
+            participants: state_builder.new_map(),
+        };
+
+        // whitelisting
+        let participants = vec![
+            (acc1, Prior::TOP),
+            (acc2, Prior::SECOND),
+            (acc3, Prior::SECOND),
+        ];
+        for v in &participants {
+            state.whitelisting(&Address::from(v.0), v.1.clone());
+        }
+
+        // Track changes in state
+        let mut expected_participants = state_builder.new_map();
+
+        let room = 2;
+        for (k, v) in participants.iter().enumerate() {
+            let deposit_usdc = if k < room {
+                ContractTokenAmount::from(5_000_000 * 200 * TARGET_UNITS as u64)
+            } else {
+                ContractTokenAmount::from(0u64)
+            };
+            let win_units = if k < room { 1 } else { 0 };
+
+            expected_participants.insert(
+                Address::from(v.0),
+                UserState {
+                    prior: v.1.clone(),
+                    deposit_usdc,
+                    tgt_units: TARGET_UNITS,
+                    win_units,
+                    claimed_inc: 0,
+                },
+            );
+        }
+
+        let expected_state = State {
+            usdc_contract: USDC,
+            proj_admin: PJ_ADMIN_ACC,
+            status: SaleStatus::Ready,
+            paused: false,
+            addr_ovl: ADDR_OVL,
+            addr_bbb: ADDR_BBB,
+            ovl_claimed_inc: 0,
+            bbb_claimed_inc: 0,
+            project_token: None,
+            schedule: def_sale_schedule(),
+            saleinfo: def_sale_info(2),
+            participants: expected_participants,
+        };
+
+        let mut host = TestHost::new(state, state_builder);
+
+        // Deposit from user1
+        let params = OnReceivingCis2Params {
+            token_id: TokenIdUnit(),
+            amount: ContractTokenAmount::from(5_000_000 * 200 * TARGET_UNITS as u64),
+            from: Address::from(acc1),
+            data: AdditionalData::empty(),
+        };
+        let params_bytes = to_bytes(&params);
+        let ctx = receive_context(
+            OVL_TEAM_ACC,
+            acc1,
+            Address::from(USDC),
+            Timestamp::from_timestamp_millis(15),
+            &params_bytes,
+        );
+        let result: ContractResult<()> = contract_user_deposit(&ctx, &mut host);
+        claim!(result.is_ok(), "Results in rejection");
+
+        // Deposit from user2
+        let params = OnReceivingCis2Params {
+            token_id: TokenIdUnit(),
+            amount: ContractTokenAmount::from(5_000_000 * 200 * TARGET_UNITS as u64),
+            from: Address::from(acc2),
+            data: AdditionalData::empty(),
+        };
+        let params_bytes = to_bytes(&params);
+        let ctx = receive_context(
+            OVL_TEAM_ACC,
+            acc2,
+            Address::from(USDC),
+            Timestamp::from_timestamp_millis(25),
+            &params_bytes,
+        );
+        let result: ContractResult<()> = contract_user_deposit(&ctx, &mut host);
+        claim!(result.is_ok(), "Results in rejection");
+
+        // Deposit from user3 - not allowed
+        let params = OnReceivingCis2Params {
+            token_id: TokenIdUnit(),
+            amount: ContractTokenAmount::from(5_000_000 * 200 * TARGET_UNITS as u64),
+            from: Address::from(acc3),
+            data: AdditionalData::empty(),
+        };
+        let params_bytes = to_bytes(&params);
+        let ctx = receive_context(
+            OVL_TEAM_ACC,
+            acc3,
+            Address::from(USDC),
+            Timestamp::from_timestamp_millis(25),
+            &params_bytes,
+        );
+        let result: ContractResult<()> = contract_user_deposit(&ctx, &mut host);
+        claim_eq!(
+            result.expect_err_report("user deposit should reject"),
+            CustomContractError::AlreadySaleClosed.into(),
+            "should reject with AlreadySaleClosed"
+        );
+
+        claim_eq!(
+            *host.state(),
+            expected_state,
+            "state has been changed unexpectedly..."
+        );
+    }
 }
