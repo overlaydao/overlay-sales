04ecf6895f9656786a627c8ae7074661bf9e3536
daratao <newsnow.bck@gmail.com>
2023-06-16 10:45:49 +0900

    repair whitelisting params

--- ovl-sale-usdc-public/src/lib.rs
+++ ovl-sale-usdc-public/src/lib.rs
@@ -174,7 +174,7 @@ struct AllowedUserParams {
 #[receive(
     contract = "pub_rido_usdc",
     name = "whitelisting",
-    parameter = "Vec<AllowedUserParams>",
+    parameter = "WhitelistingParams",
     error = "ContractError",
     mutable
 )]
